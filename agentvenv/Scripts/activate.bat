4831ec76e66d6532986db20b8518f8312658ce20
ziaulislam-mughal <ziaulislammughal0@gmail.com>
2026-04-09 19:59:55 +0500

    Install Packages and compeleted Setup for working in Aiagents

--- agentvenv/Scripts/activate.bat
+++ agentvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Administrator\Desktop\ai agent\ai-agents-for-beginners-langchain\agentvenv
+set "VIRTUAL_ENV=H:\Projects\ai agent\ai-agents-LangGraph\agentvenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(agentvenv) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=(agentvenv) "
 
 :END
 if defined _OLD_CODEPAGE (
